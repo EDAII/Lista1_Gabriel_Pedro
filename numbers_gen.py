8a16351383c0e04927a7432b907e16b847edc344
Pedro Igor <pedroigor@aluno.unb.br>
2019-08-26 16:42:13 -0300

    Reduzindo o tamanho dos números aleatórios.

--- numbers_gen.py
+++ numbers_gen.py
@@ -2,7 +2,7 @@ from random import randint
 def numbers_gen():
     number_list = []
     while(len(number_list) < 105):
-        n = int(randint(0, 100000)*randint(0, 1000)/100)
+        n = int(randint(0, 10000)*randint(0, 100)/100)
         if not n in number_list:
             number_list.append(n)
     number_list.sort()
