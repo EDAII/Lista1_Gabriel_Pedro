bc75fa5d2271231d8f413f634e9a68b9f46814e7
Pedro Igor <pedroigor@aluno.unb.br>
2019-08-26 16:36:38 -0300

    Corrigindo contagem de passos na função de busca indexada.

--- find.py
+++ find.py
@@ -8,13 +8,21 @@ def binary_search(element, vetor, steps = 0):
         return binary_search(element, vetor[metade:], steps)
     else:
         return steps
+
+
 def index_search(element, vetor):
     i = 0
+    steps = 0
     index = []
     while i < 105:
         index.append(vetor[i])
+        if element < vetor[i]:
+            break
+        steps += 1
         i+=7
     pos = 0
+    metade = int(len(index) / 2)
+
     for i in index:
         if element < i:
             break
@@ -22,7 +30,7 @@ def index_search(element, vetor):
             return 1
         else:
             pos+=1
-    steps = 0
+
     for v in vetor[(pos*7)-7:pos*7]:
         if v == element:
             return steps
