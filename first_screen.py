c5d73de423d08a26128b3b13365a578a94a66a3f
Pedro Igor <pedroigor@aluno.unb.br>
2019-08-29 09:09:06 -0300

    Adding instructions in first screen.

--- first_screen.py
+++ first_screen.py
@@ -16,6 +16,20 @@ def begin():
     start_button_skin.fill((255, 255, 255))
     start_count = 0
     clock = pygame.time.Clock()
+
+    # INSTRUCTIONS
+    ins_font = pygame.font.SysFont('bold', 25)
+    ins_texts = []
+    ins_texts.append(ins_font.render("Como jogar:", False, (255, 255, 255)))
+    ins_texts.append(ins_font.render("Seu objetivo é encontrar uma carta antes do computador.", False, (255, 255, 255)))
+    ins_texts.append(ins_font.render("As cartas estão organizadas na seguinte ordem: ", False, (255, 255, 255)))
+    ins_texts.append(ins_font.render("A ordem é crescente porém a distribuição é aleatória.", False, (255, 255, 255)))
+
+
+
+
+
+
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -29,8 +43,17 @@ def begin():
         screen.fill((150, 65, 200))
         text_surface = my_font.render("Where is the Card?", False, (255, 255, 255))
         start_text_surface = start_font.render("START", False, (255, 0, 0))
-        screen.blit(text_surface, (50,50))
+        screen.blit(text_surface, (80,10))
         screen.blit(start_button_skin, (599, 390))
+        #SHOW INSTRUCTIONS:
+        image = pygame.image.load('game.png')
+        image = pygame.transform.scale(image, (440, 320))
+        pos_y = 60
+        for ins in ins_texts[:-1]:
+            screen.blit(ins, (100,pos_y))
+            pos_y+=20
+        screen.blit(image, (130,120))
+        screen.blit(ins_texts[-1], (100, 442))
         if start_count > 100:
             screen.blit(start_text_surface, (600, 400))
         if start_count > 200:
